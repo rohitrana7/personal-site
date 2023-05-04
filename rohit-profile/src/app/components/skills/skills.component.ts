174043848cab2ad63e8eddaa003a9504d37b04ff
rohitrana7 <=>
2023-05-04 15:17:18 +0530

    code formatted

--- rohit-profile/src/app/components/skills/skills.component.ts
+++ rohit-profile/src/app/components/skills/skills.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -6,23 +12,22 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
   styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
-
   @ViewChild('skillWidth') skillWidth!: ElementRef;
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
-  skills = { 
+  skills = {
     concepts: [
       { name: 'OOPS', percentValue: '80%', value: 80 },
       { name: 'Data Structure', percentValue: '60%', value: 60 },
       { name: 'Algorithm', percentValue: '60%', value: 60 },
       { name: 'DBMS', percentValue: '70%', value: 70 },
       { name: 'Design Patterns', percentValue: '60%' },
-      { name: 'OOAD', percentValue: '55%', value: 55  },
-      { name: 'Microservices', percentValue: '70%', value: 70  },
-      { name: 'Agile Development', percentValue: '70%', value: 70  },
+      { name: 'OOAD', percentValue: '55%', value: 55 },
+      { name: 'Microservices', percentValue: '70%', value: 70 },
+      { name: 'Agile Development', percentValue: '70%', value: 70 },
     ],
     serverSide: [
-      { name: 'Spring Boot', percentValue: '65%', value: 65},
+      { name: 'Spring Boot', percentValue: '65%', value: 65 },
       { name: 'Hibernate/JPA', percentValue: '65%', value: 65 },
       { name: 'Apache Kafka', percentValue: '40%', value: 40 },
     ],
@@ -38,9 +43,7 @@ export class SkillsComponent {
       { name: 'AWS', percentValue: '45%', value: 45 },
       { name: 'Heroku', percentValue: '35%', value: 35 },
     ],
-    programmingLanguages: [
-      { name: 'Java', percentValue: '70%', value: 70 },
-    ],
+    programmingLanguages: [{ name: 'Java', percentValue: '70%', value: 70 }],
     web: [
       { name: 'HTML5', percentValue: '75%', value: 75 },
       { name: 'CSS3', percentValue: '65%', value: 65 },
@@ -52,22 +55,16 @@ export class SkillsComponent {
       { name: 'MySQL', percentValue: '75%', value: 75 },
       { name: 'SQL/ PLSQL', percentValue: '60%', value: 60 },
     ],
-    nosql :[
-      {name: 'MangoDB', percentValue: '50%', value: 50}
+    nosql: [{ name: 'MangoDB', percentValue: '50%', value: 50 }],
+    searchEngines: [{ name: 'Elasticsearch', percentValue: '50%', value: 50 }],
+    versionControlSystem: [
+      { name: 'Git/GitHub', percentValue: '75%', value: 75 },
+      { name: 'TFS', percentValue: '70%', value: 70 },
     ],
-    searchEngines :[
-      {name: 'Elasticsearch', percentValue: '50%', value: 50 }
+    ide: [
+      { name: 'Eclipse/STS', percentValue: '70%', value: 70 },
+      { name: 'Visual Studio Code', percentValue: '85%', value: 85 },
+      { name: 'IntelliJ', percentValue: '65%', value: 65 },
     ],
-    versionControlSystem:[
-      {name: 'Git/GitHub', percentValue: '75%', value: 75},
-      {name: 'TFS', percentValue: '70%', value: 70}
-    ],
-    ide:[
-      {name: 'Eclipse/STS', percentValue: '70%', value: 70},
-      {name: 'Visual Studio Code', percentValue: '85%', value: 85},
-      {name: 'IntelliJ', percentValue: '65%', value: 65}
-
-    ],
-
   };
 }
