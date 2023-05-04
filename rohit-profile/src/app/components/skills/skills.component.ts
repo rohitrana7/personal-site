d9c8c219edc7b4854fcd397f4f8e0c76ee00ac04
rohitrana7 <=>
2023-05-04 15:16:04 +0530

    dynamic skills code implemented

--- rohit-profile/src/app/components/skills/skills.component.ts
+++ rohit-profile/src/app/components/skills/skills.component.ts
@@ -1,16 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
 
-  constructor() { }
+  @ViewChild('skillWidth') skillWidth!: ElementRef;
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
-  concepts = [
-    {name: 'OOPS', value: 80, style: 'width: 80%'}
-  ]
+  skills = { 
+    concepts: [
+      { name: 'OOPS', percentValue: '80%', value: 80 },
+      { name: 'Data Structure', percentValue: '60%', value: 60 },
+      { name: 'Algorithm', percentValue: '60%', value: 60 },
+      { name: 'DBMS', percentValue: '70%', value: 70 },
+      { name: 'Design Patterns', percentValue: '60%' },
+      { name: 'OOAD', percentValue: '55%', value: 55  },
+      { name: 'Microservices', percentValue: '70%', value: 70  },
+      { name: 'Agile Development', percentValue: '70%', value: 70  },
+    ],
+    serverSide: [
+      { name: 'Spring Boot', percentValue: '65%', value: 65},
+      { name: 'Hibernate/JPA', percentValue: '65%', value: 65 },
+      { name: 'Apache Kafka', percentValue: '40%', value: 40 },
+    ],
+    buildTools: [
+      { name: 'Maven', percentValue: '55%', value: 55 },
+      { name: 'Gradle', percentValue: '45%', value: 45 },
+    ],
+    unitTesting: [
+      { name: 'JUnit/Mockito', percentValue: '65%', value: 65 },
+      { name: 'Karma/Jasmine', percentValue: '75%', value: 75 },
+    ],
+    cloud: [
+      { name: 'AWS', percentValue: '45%', value: 45 },
+      { name: 'Heroku', percentValue: '35%', value: 35 },
+    ],
+    programmingLanguages: [
+      { name: 'Java', percentValue: '70%', value: 70 },
+    ],
+    web: [
+      { name: 'HTML5', percentValue: '75%', value: 75 },
+      { name: 'CSS3', percentValue: '65%', value: 65 },
+      { name: 'Javascript', percentValue: '75%', value: 75 },
+      { name: 'Typescript', percentValue: '65%', value: 65 },
+      { name: 'Angular 9/11/13', percentValue: '75%', value: 75 },
+    ],
+    rdbms: [
+      { name: 'MySQL', percentValue: '75%', value: 75 },
+      { name: 'SQL/ PLSQL', percentValue: '60%', value: 60 },
+    ],
+    nosql :[
+      {name: 'MangoDB', percentValue: '50%', value: 50}
+    ],
+    searchEngines :[
+      {name: 'Elasticsearch', percentValue: '50%', value: 50 }
+    ],
+    versionControlSystem:[
+      {name: 'Git/GitHub', percentValue: '75%', value: 75},
+      {name: 'TFS', percentValue: '70%', value: 70}
+    ],
+    ide:[
+      {name: 'Eclipse/STS', percentValue: '70%', value: 70},
+      {name: 'Visual Studio Code', percentValue: '85%', value: 85},
+      {name: 'IntelliJ', percentValue: '65%', value: 65}
 
+    ],
+
+  };
 }
