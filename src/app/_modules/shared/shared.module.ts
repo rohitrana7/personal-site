cc86c6b2c9635e32ad6e33057f0933a5ab4903bf
Rohit Rana <rohit.rana0207@gmail.com>
2024-07-27 16:21:35 +0530

    removed interview and success build

--- src/app/_modules/shared/shared.module.ts
+++ src/app/_modules/shared/shared.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatTableModule} from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSortModule } from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator'
-import {MatCheckboxModule} from '@angular/material/checkbox';
 import { ExpandRowDirective } from '../../directives/expand-row.directive';
 
 @NgModule({
@@ -19,24 +13,12 @@ import { ExpandRowDirective } from '../../directives/expand-row.directive';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatTableModule,
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatSortModule, 
-    MatPaginatorModule,
-    MatCheckboxModule
   ],
   exports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatTableModule,
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatSortModule, 
-    MatPaginatorModule,
-    MatCheckboxModule
   ]
 })
 export class SharedModule { }
